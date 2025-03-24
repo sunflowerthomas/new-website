b12f9ce63fabcf8cd531a978def7ca3d6d0082b7
sunflowerthomas <t.man.sunflower@gmail.com>
2025-03-24 18:51:33 +0000

    bruh 💀

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'sunflowerthomas.github.io',
+  url: 'https://sunflowerthomas.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/new-website',
